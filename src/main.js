76a7053bfb5da90fc1c36bbad950b0f38490e768
Bastien Sarolea <bastien.sarolea@gmail.com>
2023-11-16 14:54:40 +0100

    33

--- src/main.js
+++ src/main.js
@@ -50,53 +50,38 @@ import { Push2Web } from "@snap/push2web";
     //var cameraKit = await bootstrapCameraKit({ apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjk4NDEyNDI1LCJzdWIiOiIxMzk1NDk4MC1hYjQwLTQwMTAtYThhZi02NmI5NWYyM2RlYmR-U1RBR0lOR34xOTcxMTQ2OC1jZTY3LTQ5OTgtYmQ5ZS0xNzAwNTRkYTk5NzgifQ.WzqacKQZQIh5SUMC7V45ndhVsk8jjI3BxiwhQVetkz4' })
 
 //V2 working here
-    // var cameraKit = await bootstrapCameraKit({ apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjk4NDEyNDI1LCJzdWIiOiIxMzk1NDk4MC1hYjQwLTQwMTAtYThhZi02NmI5NWYyM2RlYmR-U1RBR0lOR34xOTcxMTQ2OC1jZTY3LTQ5OTgtYmQ5ZS0xNzAwNTRkYTk5NzgifQ.WzqacKQZQIh5SUMC7V45ndhVsk8jjI3BxiwhQVetkz4' }, (container) =>
-    //     container.provides(
-    //         Injectable(
-    //             remoteApiServicesFactory.token,
-    //             [remoteApiServicesFactory.token],
-    //             (existing) => [...existing, damsService]
-    //         )
-    //     )
-    // );
+    var cameraKit = await bootstrapCameraKit({ apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjk4NDEyNDI1LCJzdWIiOiIxMzk1NDk4MC1hYjQwLTQwMTAtYThhZi02NmI5NWYyM2RlYmR-U1RBR0lOR34xOTcxMTQ2OC1jZTY3LTQ5OTgtYmQ5ZS0xNzAwNTRkYTk5NzgifQ.WzqacKQZQIh5SUMC7V45ndhVsk8jjI3BxiwhQVetkz4' }, (container) =>
+        container.provides(
+            Injectable(
+                remoteApiServicesFactory.token,
+                [remoteApiServicesFactory.token],
+                (existing) => [...existing, damsService]
+            )
+        )
+    );
 
-    //PUSH2WEB
-    const push2Web = new Push2Web();
-    push2Web.events.addEventListener("lensReceived", console.info);
-    push2Web.events.addEventListener("error", console.error);
-    push2Web.events.addEventListener("subscriptionChanged", console.info);
-
-    const extensions = (container) => container.provides(push2Web.extension);
-
-    const cameraKit = await bootstrapCameraKit({ apiToken: "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjk4NDEyNDI1LCJzdWIiOiIxMzk1NDk4MC1hYjQwLTQwMTAtYThhZi02NmI5NWYyM2RlYmR-U1RBR0lOR34xOTcxMTQ2OC1jZTY3LTQ5OTgtYmQ5ZS0xNzAwNTRkYTk5NzgifQ.WzqacKQZQIh5SUMC7V45ndhVsk8jjI3BxiwhQVetkz4" }, extensions);
-    const cameraKitSession = await cameraKit.createSession();
-
-        //push2web suite
-        push2Web.subscribe("49beb0c1-28e4-43a7-96f3-1c4bfc0dad08", cameraKitSession, cameraKit.lensRepository.loadLensGroups(['a807b90b-4b77-4def-a142-495d0636d1f5']));
-
-    //end PUSH2WEB
     
     
     
-    // const session = await cameraKit.createSession();
-    // document.getElementById('canvas').replaceWith(session.output.live);
-    // const { lenses } = await cameraKit.lensRepository.loadLensGroups(['a807b90b-4b77-4def-a142-495d0636d1f5']);
-    // session.applyLens(lenses[0]);
-    // // let mediaStream = await navigator.mediaDevices(getUserMedia({ video: true }));
-    // let mediaStream = await navigator.mediaDevices.getUserMedia({
-    //     video: {
-    //         facingMode: 'environment'
-    //     }
-    // });
-    // const source = createMediaStreamSource(mediaStream, {
-    //     // transform: Transform2D.MirrorX,
-    //     fpsLimit: 30,
-    //     cameraType: 'back',
-    // });
-    // await session.setSource(source)
-    // session.setSource(source)
-    // session.source.setRenderSize(window.innerWidth, window.innerHeight)
-    // session.play();
+    const session = await cameraKit.createSession();
+    document.getElementById('canvas').replaceWith(session.output.live);
+    const { lenses } = await cameraKit.lensRepository.loadLensGroups(['a807b90b-4b77-4def-a142-495d0636d1f5']);
+    session.applyLens(lenses[0]);
+    // let mediaStream = await navigator.mediaDevices(getUserMedia({ video: true }));
+    let mediaStream = await navigator.mediaDevices.getUserMedia({
+        video: {
+            facingMode: 'environment'
+        }
+    });
+    const source = createMediaStreamSource(mediaStream, {
+        // transform: Transform2D.MirrorX,
+        fpsLimit: 30,
+        cameraType: 'back',
+    });
+    await session.setSource(source)
+    session.setSource(source)
+    session.source.setRenderSize(window.innerWidth, window.innerHeight)
+    session.play();
 
 
 
