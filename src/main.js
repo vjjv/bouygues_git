83554339d96605b3a6f41d95f48f5198f50e1e1e
Bastien Sarolea <sarobastien@gmail.com>
2023-12-18 15:40:09 +0100

    1

--- src/main.js
+++ src/main.js
@@ -122,7 +122,6 @@ import {
         //     height: 1
         // }).replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
         // window.location.href=image; // it will save locally
-
     },2000)
 
 
