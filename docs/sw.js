8a6f4c5ad80020a413b863ec6e323a2e18495fe9
Bastien Sarolea <sarobastien@gmail.com>
2024-10-11 13:51:34 +0200

    03

--- docs/sw.js
+++ docs/sw.js
@@ -1,35 +1,3 @@
-// sw.js
-
-
-// Handle fetch events in Service Worker
-self.addEventListener("fetch", (event) => {
-    if (event.request.method === "POST") {
-        event.respondWith(
-            fetch(event.request).then((response) => {
-                const clonedResponse = response.clone();
-                clonedResponse.json().then(data => {
-                    storeResponse(event.request.url, data);
-                });
-                return response; // Return the original response
-            })
-        );
-    } else if (event.request.method === "GET") {
-        event.respondWith(
-            getCachedResponse(event.request.url).then(cachedData => {
-                if (cachedData) {
-                    return new Response(JSON.stringify(cachedData), { status: 200 });
-                } else {
-                    return fetch(event.request).catch(() => {
-                        // Fallback to an offline page if needed
-                        return caches.match('/offline.html');
-                    });
-                }
-            })
-        );
-    }
-});
-
-
 // app.js
 
 // IndexedDB setup
@@ -87,3 +55,36 @@ function getCachedResponse(url) {
         });
     });
 }
+
+// sw.js
+
+
+// Handle fetch events in Service Worker
+self.addEventListener("fetch", (event) => {
+    if (event.request.method === "POST") {
+        event.respondWith(
+            fetch(event.request).then((response) => {
+                const clonedResponse = response.clone();
+                clonedResponse.json().then(data => {
+                    storeResponse(event.request.url, data);
+                });
+                return response; // Return the original response
+            })
+        );
+    } else if (event.request.method === "GET") {
+        event.respondWith(
+            getCachedResponse(event.request.url).then(cachedData => {
+                if (cachedData) {
+                    return new Response(JSON.stringify(cachedData), { status: 200 });
+                } else {
+                    return fetch(event.request).catch(() => {
+                        // Fallback to an offline page if needed
+                        return caches.match('/offline.html');
+                    });
+                }
+            })
+        );
+    }
+});
+
+
